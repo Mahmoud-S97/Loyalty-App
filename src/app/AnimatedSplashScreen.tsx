d001bfb6ad206b654766638a03ba27d94a580d40
Mahmoud Saleh <mahmoudsaleh@MACBOOKPRO-5870.station>
2025-12-29 02:27:29 +0000

    Feat(Splash-Screen): Added animated Splash-Screen

---
+++ src/app/AnimatedSplashScreen.tsx
@@ -0,0 +1,27 @@
+import LottieView from 'lottie-react-native';
+import React from 'react';
+import { View } from 'react-native';
+
+type Props = {
+    onFinish: () => void;
+}
+
+const AnimatedSplashScreen = ({ onFinish }: Props) => {
+
+    return (
+        <View className='flex-1 items-center justify-center bg-light-100'>
+            <LottieView
+                source={require('../assets/lottie/barbers-pole.json')}
+                autoPlay
+                loop={false}
+                onAnimationFinish={onFinish}
+                style={{
+                    width: 200,
+                    height: 200
+                }}
+            />
+        </View>
+    );
+}
+
+export default AnimatedSplashScreen;
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,23 @@
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import AnimatedSplashScreen from './AnimatedSplashScreen';
 import './global.css';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+
+  const [showSplash, setShowSplash] = useState<boolean>(true);
+
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  if (showSplash) {
+    return <AnimatedSplashScreen onFinish={() => setShowSplash(false)} />
+  }
 
   return (
     <SafeAreaProvider>
