2f0853e5f8bc7cc55a5f4c1ce97698c5596e05ea
Mahmoud Saleh <mahmoudsaleh@MACBOOKPRO-5870.station>
2026-03-31 00:58:30 +0100

    Util(getFontWeight): Added getFontWeight util-function for app language

--- src/components/ui/content/AppText.tsx
+++ src/components/ui/content/AppText.tsx
@@ -2,21 +2,24 @@ import React, { JSX } from 'react';
 import { Text } from 'react-native';
 import { cn } from '@/lib/nativeWindCSS/cn';
 import { getTranslated } from '@/lib/localization';
+import { getFontWeight, is_RTL } from '@/utils';
 
 type AppTextProps = {
-    children: any,
-    numberOfLines?: number,
-    className?: string,
-    withTranslation?: boolean
+  children: any,
+  numberOfLines?: number,
+  className?: string,
+  withTranslation?: boolean,
+  weight?: string
 }
 
-const AppText = ({ children, numberOfLines, className, withTranslation = true }: AppTextProps): JSX.Element => {
+const AppText = ({ children, numberOfLines, className, withTranslation = true, weight = 'regular' }: AppTextProps): JSX.Element => {
 
-    const renderChildren = withTranslation ? getTranslated(children) : children;
+  const getFontWeightClases = getFontWeight(weight);
+  const renderChildren = withTranslation ? getTranslated(children) : children;
 
-    return (
-        <Text testID='AppText:Text' numberOfLines={numberOfLines} className={cn('text-neutral-900 dark:text-neutral-500 font-normal text-base', className)}>{renderChildren}</Text>
-    )
+  return (
+    <Text testID='AppText:Text' numberOfLines={numberOfLines} className={cn('text-neutral-900 dark:text-neutral-500 font-normal text-base', getFontWeightClases, className)}>{renderChildren}</Text>
+  )
 }
 
 export default AppText;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,23 @@ export const is_IOS = (): boolean => {
 export const is_RTL = (): boolean => {
   return I18nManager.isRTL;
 }
+
+// Getting the font-weight based on the App language
+export const getFontWeight = (weight: string): string => {
+  switch (weight) {
+    case 'regular':
+      return is_RTL() ? 'font-cairo-reg' : 'font-inter-reg';
+      break;
+    case 'medium':
+      return is_RTL() ? 'font-cairo-md' : 'font-inter-md';
+      break;
+    case 'semiBold':
+      return is_RTL() ? 'font-cairo-semi-b' : 'font-inter-semi-b';
+      break;
+    case 'bold':
+      return is_RTL() ? 'font-cairo-b' : 'font-inter-b';
+      break;
+    default:
+      return is_RTL() ? 'font-cairo-reg' : 'font-inter-reg';
+  }
+}
